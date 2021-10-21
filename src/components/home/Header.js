1c9f1664079785a80e2903ebfe919a9ae4d5deb9
Rohit Mali <malirohit243@gmail.com>
2021-10-21 22:12:32 +0530

    Header styling applied

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
 const Header = () => {
   return (
     <div className='header'>
@@ -11,17 +11,27 @@ const Header = () => {
       <div className='card-container'>
         <div className='card'>
           <img className='card-image' src='/images/simpl.svg' alt='' />
-          <p>Spent till now</p>
-          <p>₹1800</p>
-          <div>
-            <p>Bill overdue</p>
-          </div>
-          <p>Due date</p>
-          <p>15th August</p>
-          <button>Pay 1500</button>
+          <p className='card-spent'>Spent till now</p>
+          <p className='card-amount'>₹1800</p>
+
+          <p className='card-bill-text'>BILL OVERDUE</p>
+          <p className='card-dd-text'>DUE DATE</p>
+          <p className='card-dd'>15 Aug</p>
+          <button className='card-button'>
+            <span className='card-button-pay'>PAY ₹1500</span>
+          </button>
         </div>
         <div className='card'>
-          <img src='/images/simpl.svg' alt='' />
+          <img className='card-image-2' src='/images/paylater.png' alt='' />
+          <p className='card-spent'>Spent This Month</p>
+          <p className='card-amount'>₹1150</p>
+          <div>
+            <AiOutlineQuestionCircle className='card-icon' />
+            <a className='card-link' href='#'>
+              FAQ
+            </a>
+            <div className='card-desc'></div>
+          </div>
         </div>
       </div>
     </div>
